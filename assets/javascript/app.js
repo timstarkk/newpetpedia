f62e7c1f4666a607949c29dc7a1ae5a088586d9a
timstarkk <timstarkk@icloud.com>
2019-09-12 20:05:10 -0500

    got API working. added search button and tokenfile

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,16 @@
-var data = {
-    "grant_type": "client_credentials",
-    "client_id": "qdbZnyx35Eucht2eGTONDw6UlgjtwO2EoSp8u8ZSBwGurYa2ba",
-    "client_secret": "JAVzjeOIao4gdrvMT4tQUDUGh1Lu06XGTw9HWYwn"
-};
+$(document).ready(function () {
+    $('#search-button').click(function (event) {
+        event.preventDefault();
 
-// var url = "https://api.petfinder.com/v2/oauth2/qdbZnyx35Eucht2eGTONDw6UlgjtwO2EoSp8u8ZSBwGurYa2ba"
-// var url = "https://cors.io/?http://https://api.petfinder.com/v2/animals"
-var url = "https://api.petfinder.com/v2/animals"
+        var pf = new petfinder.Client({ apiKey: "DEEgmJQqmtdmPQKmF6fVKXW9EzOjUR4EBMkSkX5DqyapJ1keTP", secret: "Aa7kiPZFw71Z5YWnOUosFbNtXvPaELvcbV0t6On2" });
 
-function callPetfinder() {
-    $.ajax({
-        url,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        method: "GET",
-    }).then(function (response) {
-        data = response.data;
-        console.log(data);
+        pf.animal.search()
+            .then(function (response) {
+                console.log(response)
+            })
+            .catch(function (error) {
+                // Handle the error
+            });
+
     })
-}
-
-callPetfinder();
+})
