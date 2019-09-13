b61b2f42887c27121150ff3e5549376520fd41be
timstarkk <timstarkk@icloud.com>
2019-09-12 21:26:50 -0500

    refined search parameters to work &breed dynamics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,41 @@
+let type = "";
+let city = "";
+let state = "";
+let breed = "";
+
 $(document).ready(function () {
-    $('#search-button').click(function (event) {
+    $('.search').click(function (event) {
         event.preventDefault();
+        type = this.id;
+        $('#breed').html(`<option selected>Choose...</option>`);
 
         var pf = new petfinder.Client({ apiKey: "DEEgmJQqmtdmPQKmF6fVKXW9EzOjUR4EBMkSkX5DqyapJ1keTP", secret: "Aa7kiPZFw71Z5YWnOUosFbNtXvPaELvcbV0t6On2" });
 
-        pf.animal.search()
+        pf.animalData.breeds(`${type}`)
+            .then(response => {
+                console.log(response);
+
+                for (i = 0; i < response.data.breeds.length; i++) {
+
+                    $('#breed').append(`<option>${response.data.breeds[i].name}</option>`)
+                }
+            });
+    })
+
+    $('#search-button').click(function (event) {
+        event.preventDefault();
+        city = $('#city').val().trim();
+        state = $('#state').val();
+        breed = $('#breed').val();
+
+        console.log(type, city, state, breed);
+
+        var pf = new petfinder.Client({ apiKey: "DEEgmJQqmtdmPQKmF6fVKXW9EzOjUR4EBMkSkX5DqyapJ1keTP", secret: "Aa7kiPZFw71Z5YWnOUosFbNtXvPaELvcbV0t6On2" });
+
+        pf.animal.search({ type: type, breed: breed, location: `${city}, ${state}` })
             .then(function (response) {
                 console.log(response)
+
             })
             .catch(function (error) {
                 // Handle the error
