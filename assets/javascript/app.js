1d19d9facd1a8e913f26c31a4ebb42535c3fe5e4
timstarkk <timstarkk@icloud.com>
2019-09-14 10:49:49 -0500

    created dynamic result divs and added small img

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,8 @@ let city = "";
 let state = "";
 let breed = "";
 
+const resultIds = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty']
+
 $(document).ready(function () {
     $('.search').click(function (event) {
         event.preventDefault();
@@ -28,14 +30,24 @@ $(document).ready(function () {
         state = $('#state').val();
         breed = $('#breed').val();
 
-        console.log(type, city, state, breed);
-
         var pf = new petfinder.Client({ apiKey: "DEEgmJQqmtdmPQKmF6fVKXW9EzOjUR4EBMkSkX5DqyapJ1keTP", secret: "Aa7kiPZFw71Z5YWnOUosFbNtXvPaELvcbV0t6On2" });
 
         pf.animal.search({ type: type, breed: breed, location: `${city}, ${state}` })
             .then(function (response) {
                 console.log(response)
 
+                for (i = 0; i < response.data.animals.length; i++) {
+
+                    // console.log(response.data.animals[0].photos[0].small);
+
+
+                    $('.mainRow').append(`<div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 resultBox" id="${resultIds[i]}">
+
+                    <img src="" alt="${response.data.animals[0].photos[0].small}"></img>
+
+
+                    </div>`)
+                }
             })
             .catch(function (error) {
                 // Handle the error
